37e3753284601b8e6465e555da918d2e6ab5aa54
Luli-blip <wendygatsheni@icloud.comgit config --global user.name Luli-blipgit config --global user.email wendygatsheni@icloud.com>
2023-06-21 18:24:17 -0700

    Like, comment, subscribe

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,23 +5,24 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Returns largest number
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a >= b && a >= c)
-{
-largest = a;
-}
-else if (b >= a && b >= c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
