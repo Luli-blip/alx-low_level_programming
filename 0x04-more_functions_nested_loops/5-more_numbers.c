926c991d03b01e9e596d489ada1f8ca57e2836d9
Luli-blip <wendygatsheni@icloud.comgit config --global user.name Luli-blipgit config --global user.email wendygatsheni@icloud.com>
2023-06-22 15:59:02 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,26 @@
 #include "main.h"
-
 /**
- * more_numbers - print numbers from 0-14 10 times
+ * more_numbers - Prints numbers from 0 through 14, 10 times
  * Return: void
  */
-
 void more_numbers(void)
 {
+	int n;
+	int i;
+	int div;
+	int rem;
 
-int i, j;
-	for (i = 1; i <= 10; i++)
+	for (n = 0; n < 10; n++)
 	{
-		if (j >= 10)
-		_putchar('1');
-		_putchar(j % 10 + '0');
-	}
+		for (i = 0; i < 15; i++)
+		{
+			rem = i % 10;
+			div = i / 10;
+			if (i > 9)
+				_putchar(div + '0');
+			_putchar(rem + '0');
+
+		}
 		_putchar('\n');
 	}
 }
